fba0b24231a341c1d1acfe1d221c0b48accd1adc
phaneendra.yedidi <phaneendra.yedidi@hexagon.com>
2026-02-01 17:51:28 +0530

    fixed challenges

--- src/features/quiz/challenge.service.ts
+++ src/features/quiz/challenge.service.ts
@@ -1,21 +1,21 @@
-import api from "../../api/client";
+import { apiClient } from "../../api/client";
 
 export const createChallenge = async () => {
-    const res = await api.post("/challenges/create");
+    const res = await apiClient.post("/challenges/create");
     return res.data;
 };
 
 export const joinChallenge = async (code: string) => {
-    const res = await api.post("/challenges/join", { code });
+    const res = await apiClient.post("/challenges/join", { code });
     return res.data;
 };
 
 export const getChallenge = async (id: string) => {
-    const res = await api.get(`/challenges/${id}`);
+    const res = await apiClient.get(`/challenges/${id}`);
     return res.data;
 };
 
 export const submitChallengeScore = async (id: string, score: number) => {
-    const res = await api.post(`/challenges/${id}/submit`, { score });
+    const res = await apiClient.post(`/challenges/${id}/submit`, { score });
     return res.data;
 };
